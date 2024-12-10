ffd5e2a542bb99716c0a29c7f33f20f84c648e88
markkizik <deadmark666@gmail.com>
2024-12-10 18:34:09 +0200

    fix bugs and complete task \ last commit 10.12.2024

--- js/components/account.component.js
+++ js/components/account.component.js
@@ -2,39 +2,50 @@ const headerAccount = document.querySelector('.header__account');
 const menuUser = headerAccount.querySelector('.user-account__menu')
 
 const headerSignIn = document.querySelector('.header__sign-up');
-
 const accountUser = headerAccount.querySelector('.user-account');
+const btnSignOut = headerAccount.querySelector('.user-account-top__sign-out');
 
 const emailUser = headerAccount.querySelector('.user-account-top__email');
-
-const btnSignOut = headerAccount.querySelector('.user-account-top__sign-out')
+const firstName = headerAccount.querySelector('.user-account-top__fullname');
 
 // ----------------------------------------
-accountReg
+const USER_AUTH_KEY = 'userAuthData';
 // ----------------------------------------
+
+const getAuthData = () => {
+    const data = localStorage.getItem(USER_AUTH_KEY)
+    return JSON.parse(data);
+};
 
 function recheckAccountUser() {
-    const email = localStorage.getItem('email');
-    emailUser.innerHTML = email;
+    const dataUser = getAuthData();
 
-    if (!email) {
+    if (!dataUser) {
         headerAccount.classList.add('--is-show');
         headerSignIn.classList.remove('--is-show');
     } else {
         headerSignIn.classList.add('--is-show');
         headerAccount.classList.remove('--is-show');
+
+        dataUser.forEach(item => {
+            emailUser.innerHTML = item.email;
+            firstName.textContent = `${item.usernameFirst} ${item.usernameLast}`;
+        });
     }
 };
 
-export const accountComponent = () => {
-    recheckAccountUser();
+export const accountComponent = () => { 
+    const authData = getAuthData();
+
+    if(authData) {
+        recheckAccountUser();
+    }
 
     accountUser.addEventListener('click', () => {
         menuUser.classList.toggle('--close');
     });
 
     btnSignOut.addEventListener('click', () => {
-        localStorage.removeItem('email');
-        window.location.href = 'index.html';
+        localStorage.removeItem(USER_AUTH_KEY);
     });
 }
