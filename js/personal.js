cb8f5c715c4be446054b3e6a74742342c6d9a3e5
markkizik <deadmark666@gmail.com>
2025-01-03 23:56:44 +0200

    last commit 03.01.2025 marik

--- js/personal.js
+++ js/personal.js
@@ -3,6 +3,7 @@ const html = document.querySelector('html');
 const blockNameUser = document.querySelector('.card-personal__value [data-full-name]');
 
 const dialogPersonal = document.querySelector('.profile-form');
+
 const closeBtnDialog = document.querySelector('[data-close-modal-window]');
 
 const cardItems = document.querySelectorAll('.card-personal__item');
@@ -16,24 +17,68 @@ const getUserAuthData = () => {
     return JSON.parse(data);
 };
 
+const blockEventOnContent = (parm = false) => {
+    const main = document.querySelector('.profile-main');
+    const content = document.querySelector('.profile-main__content');
+
+    if (!parm) {
+        content.classList.remove('gm-not-event');
+        main.classList.remove('gm-scroll-hide');
+        return
+    }
+
+    main.classList.add('gm-scroll-hide');
+    content.classList.add('gm-not-event');
+};
+
+const activeInfoItem = (elm) => {
+    elm.classList.add('--active');
+};
+
+const inActiveInfoItems = () => {
+    cardItems.forEach(item => item.classList.remove('--active'));
+};
+
+const showEditForms = (activeForm) => {
+    const forms = dialogPersonal.querySelectorAll('.profile-form__section');
+
+    dialogPersonal.classList.remove('gm-hide');
+
+    forms.forEach(elm => {
+        const data = elm.dataset.targetDialog;
+
+        (data === activeForm)
+            ? elm.classList.remove('gm-hide')
+            : elm.classList.add('gm-hide')
+    });
+};
+
+const hideEditForms = () => {
+    dialogPersonal.classList.add('gm-hide');
+};
+
 cardItems.forEach(item => {
     item.addEventListener('click', () => {
-        dialogPersonal.classList.remove('gm-hide');
-        item.classList.add('--active');
-        // document.body.classList.add('gm-scroll-hide');
+        const targetElement = item.dataset.actionDialog;
+        showEditForms(targetElement);
+        blockEventOnContent(true);
+        activeInfoItem(item);
     });
 });
 
 closeBtnDialog.addEventListener('click', () => {
-    dialogPersonal.classList.add('gm-hide');
-    cardItems.classList.remove('--active');
-    // document.body.classList.remove('gm-scroll-hide');
+    hideEditForms();
+    inActiveInfoItems();
+
+    blockEventOnContent(false);
 });
 
 window.addEventListener('DOMContentLoaded', () => {
     const dataUser = getUserAuthData();
 
-    dataUser.forEach(item => {
-        blockNameUser.textContent = `${item.usernameFirst} ${item.usernameLast}`;
-    });
+    if (dataUser) {
+        dataUser.forEach(item => {
+            blockNameUser.textContent = `${item.usernameFirst} ${item.usernameLast}`;
+        });
+    }
 });
