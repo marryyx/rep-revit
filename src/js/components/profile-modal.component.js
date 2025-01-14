22c4e093930c0716ba2f2a1979e2f14b4a8a5f8e
markkizik <deadmark666@gmail.com>
2025-01-14 10:56:48 +0200

    Bug class Account Component

---
+++ src/js/components/profile-modal.component.js
@@ -0,0 +1,44 @@
+import { CLIENT_STORAGE_KEYS } from '../store/globals-params-store.js';
+
+const menuUser = document.querySelector('.user-account__menu')
+
+const accountUser = document.querySelector('[data-user-button]');
+
+const btnSignOut = document.querySelector('.user-account__btn-sign-out');
+
+const emailUser = document.querySelector('.user-account__email');
+const firstName = document.querySelector('.user-account__fullname');
+
+// ----------------------------------------
+const USER_AUTH_KEY = CLIENT_STORAGE_KEYS.userAuthData;
+// ----------------------------------------
+
+const profileModalComponent = () => {
+
+    const getAuthData = () => {
+        const data = localStorage.getItem(USER_AUTH_KEY)
+        return JSON.parse(data);
+    };
+
+    const recheckAccountUser = () => {
+        const dataUser = getAuthData();
+
+
+        dataUser.forEach(item => {
+            emailUser.innerHTML = item.email;
+            firstName.textContent = `${item.usernameFirst} ${item.usernameLast}`;
+        });
+    };
+
+    accountUser.addEventListener('click', () => {
+        menuUser.classList.toggle('gm-hide');
+    });
+
+    btnSignOut.addEventListener('click', () => {
+        localStorage.removeItem(USER_AUTH_KEY);
+    });
+
+    recheckAccountUser();
+}
+
+export { profileModalComponent }
--- src/js/components/profile.component.js
+++ src/js/components/profile.component.js
@@ -1,45 +1,32 @@
 import { CLIENT_STORAGE_KEYS } from '../store/globals-params-store.js';
+// import { Routing } from '../root/routing.js'
 
-const menuUser = document.querySelector('.user-account__menu')
-
-const accountUser = document.querySelector('[data-user-button]');
-
-const btnSignOut = document.querySelector('.user-account__btn-sign-out');
-
-const emailUser = document.querySelector('.user-account__email');
-const firstName = document.querySelector('.user-account__fullname');
+const profileMain = document.querySelector('.profile-main');
 
 // ----------------------------------------
 const USER_AUTH_KEY = CLIENT_STORAGE_KEYS.userAuthData;
 // ----------------------------------------
 
 const profileComponent = () => {
-    const getAuthData = () => {
-        const data = localStorage.getItem(USER_AUTH_KEY)
+    const getUserAuthData = () => {
+        const data = localStorage.getItem(USER_AUTH_KEY);
+
         return JSON.parse(data);
     };
-    
-    const recheckAccountUser = () => {
-        const dataUser = getAuthData();
-    
-    
+
+    const userTextContent = () => {
+        const dataUser = getUserAuthData();
+
+        if (!dataUser) return;
+
         dataUser.forEach(item => {
-            emailUser.innerHTML = item.email;
-            firstName.textContent = `${item.usernameFirst} ${item.usernameLast}`;
+            const userName = profileMain.querySelector('.profile-main__welcome [data-full-name-user]');
+
+            userName.textContent = `${item.usernameFirst} ${item.usernameLast}`;
         });
     };
-    
-    document.addEventListener('DOMContentLoaded', () => {
-        recheckAccountUser();
-    
-        accountUser.addEventListener('click', () => {
-            menuUser.classList.toggle('gm-hide');
-        });
-    
-        btnSignOut.addEventListener('click', () => {
-            localStorage.removeItem(USER_AUTH_KEY);
-        });
-    });
+
+    userTextContent();
 }
 
 export { profileComponent }
