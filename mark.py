49e0a56008b837962b43b579117d196e5747c455
marryyx <markfrancyzov@gmail.com>
2025-01-30 15:38:26 +0200

    I've taken the last step

---
+++ mark.py
@@ -0,0 +1,12 @@
+old = 15
+print("Information:")
+print("My Years:", old)
+
+name = "Mark"
+print("My name:", name)
+
+infome = "I'm a cool proger"
+print("About me:", infome)
+
+addinfo = "Ilya is the best!"
+print("Additional Information:", addinfo)
---
+++ nelly.py
@@ -0,0 +1,6 @@
+print("Little Bubochka loves:")
+
+NELLY_LIKES = "Small chicken, sushi only with cucumber, I want this, no, that!"
+
+print(NELLY_LIKES)
+
