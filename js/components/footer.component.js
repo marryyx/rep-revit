0f00a1fceaa084fcc55bda218a2dae772e661cd0
marryyx <markfrancyzov@gmail.com>
2025-01-30 15:38:45 +0200

    add footer.js + add footer mobile version \ last commit 23.11.2024

---
+++ js/components/footer.component.js
@@ -0,0 +1,23 @@
+export const footerComponent = () => {
+    function reportWindowSize() {
+        const widthWindow = window.innerWidth;
+
+        const titleTopAll = document.querySelectorAll('.footer__top');
+
+        console.log('Width Window =', widthWindow);
+
+        titleTopAll.forEach(item => {
+            item.addEventListener('click', () => {
+                const cardKey = item.dataset.showCard;
+                const card = document.querySelector(`.footer__card[data-show-card="${cardKey}"]`);
+
+                card.classList.toggle('--is-select');
+            });
+        });
+    }
+    reportWindowSize();
+
+    window.addEventListener("resize", () => {
+        reportWindowSize();
+    });
+}
--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 import { tabsComponent } from './components/tabs.component.js';
 import { basketComponent } from './components/basket.component.js';
+import { subheadComponent } from './components/subhead.component.js';
 import { videoComponent } from './components/video.component.js';
 import { assistantComponent } from './components/assistant.component.js';
 import { faq } from './components/faq.component.js';
-import { subheadComponent } from './components/subhead.component.js';
+import { footerComponent } from './components/footer.component.js';
 
 document.addEventListener("DOMContentLoaded", (event) => {
     tabsComponent();
@@ -12,5 +13,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
     videoComponent();
     assistantComponent();
     faq();
+    footerComponent()
     console.log("Page loaded✅")
 });
