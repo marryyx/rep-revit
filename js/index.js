41aa2a624d0282be526e859625aef0fcb0c3d7ad
marryyx <markfrancyzov@gmail.com>
2025-01-30 15:38:36 +0200

    Complete js array

--- js/index.js
+++ js/index.js
@@ -1,46 +1,81 @@
-let mainpush = document.querySelector('.main__push');
-Elpush = ["🔵", "🔵", "🔵", "🔵"];
+let
+    mainpush = document.querySelector('.main__push'),
+
+    Elpush = ["🔵", "🔵", "🔵", "🔵"];
+
 Elpush.push("🟣");
 
 mainpush.innerHTML = Elpush.join(' ');
 
+// *************************************
+
 let mainunshift = document.querySelector('.main__unshift');
-array2 = ["🔵", "🔵", "🔵", "🔵"];
+let array2 = ["🔵", "🔵", "🔵", "🔵"];
 array2.unshift("🟣");
 mainunshift.innerHTML = array2.join(' ');
 
+// *************************************
 
-let mainpop = document.querySelector('.main__pop');
-array3 = ["🔵", "🟢", "🟡", "🟠"];
+let
+    mainpop = document.querySelector('.main__pop'),
+    array3 = ["🔵", "🟢", "🟡", "🟠"];
 array3.pop();
 mainpop.innerHTML = array3.join(' ');
 
-let mainshift = document.querySelector('.main__shift');
-array4 = ["🔵", "🟢", "🟡", "🟠"];
+// *************************************
+
+let
+    mainshift = document.querySelector('.main__shift'),
+    array4 = ["🔵", "🟢", "🟡", "🟠"];
 array4.shift();
 mainshift.innerHTML = array4.join(' ');
 
-let mainmap = document.querySelector('.main__map');
-array5 = ["🔵", "🔵", "🔵", "🔵"];
-array5.map["🔵=>🔵=🟣"];
-mainmap.innerHTML = array5.join(' ');
+// *************************************
 
-let mainfilter = document.querySelector('.main__filter');
-const array6 = ["1", "2", "3", "4", "5", "6"];
-const arr6new = array6.filter("3");
-mainfilter.innerHTML = arr6new.join(' ');
+let
+    mainmap = document.querySelector('.main__map'),
+    array5 = ["🔵", "🔵", "🔵", "🔵"];
 
-let mainreverse = document.querySelector('.main__reverse');
-array7 = ["🔵", "🟢", "🟡", "🟠"];
+let newmap = array5.map(() => "🟣");
+    // function (elm) {
+    //     if (elm === "🔵") {
+    //         return "🟣"
+    //     }
+    //     else {
+    //         return "🔵"
+    //     }
+    // }
+mainmap.innerHTML = newmap.join(' ');
+
+// *************************************
+
+let
+    mainfilter = document.querySelector('.main__filter'),
+    array6 = ["🔵", "🟡", "🔵", "🟠"];
+let array6New = array6.filter((elm) => elm === "🔵");
+mainfilter.innerHTML = array6New.join(' ');
+
+// *************************************
+
+let
+    mainreverse = document.querySelector('.main__reverse'),
+    array7 = ["🔵", "🟢", "🟡", "🟠"];
 array7.reverse();
 mainreverse.innerHTML = array7.join(' ');
 
-let mainat = document.querySelector('.main__at');
-array8 = ["🔵", "🟢", "🟡", "🟠"];
-array8.at(2);
-mainat.innerHTML = array8.join(' ');
+// *************************************
 
-let mainslice = document.querySelector('.main__slice');
-array9 = ["🔵", "🟢", "🟡", "🟠"];
-array9.slice(2);
-mainslice.innerHTML = array9.join(' ');
+let
+    mainat = document.querySelector('.main__at'),
+    array8 = ["🔵", "🟢", "🟡", "🟠"];
+array8.at(2);
+mainat.innerHTML = array8.at(2);
+
+// *************************************
+
+let
+    mainslice = document.querySelector('.main__slice'),
+    array9 = ["🔵", "🟢", "🟡", "🟠"];
+mainslice.innerHTML = array9.slice(2).join(' ');
+
+// *************************************
