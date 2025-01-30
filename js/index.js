77353cf51a597681522c28a31276b31a8ef7ac18
marryyx <markfrancyzov@gmail.com>
2025-01-30 15:38:37 +0200

    ya ne sdelau!

--- js/index.js
+++ js/index.js
@@ -1,6 +1,42 @@
-const btn = document.querySelector('button')
-const box = document.querySelector('.box')
+const cardAll = document.querySelectorAll('.select-term__card');
+const priceCard = document.querySelector('.price-text-buy');
+const deskCard = document.querySelector('.desk-text-buy')
+const tabs = document.querySelectorAll('.buy-component__tab');
 
-btn.addEventListener("click", () => {
-    box.classList.toggle('show')
+const INFORMATION = {
+    first: '/3 years for 1 user (pay annually)',
+    second: '/year for 1 user',
+    third: '/month for 1 user'
+};
+
+const PRICE = {
+    first: '$8,730',
+    second: '$2,910',
+    third: '$365'
+};
+
+const clearSelectTab = () => {
+    tabs.forEach(element => {
+        element.classList.remove('--is-active');
+    });
+};
+
+tabs.forEach(element => {
+    element.addEventListener("click", () => {
+        clearSelectTab();
+        element.classList.add('--is-active');
+    });
+});
+
+const clearSelectCard = () => {
+    cardAll.forEach(element => {
+        element.classList.remove('--is-select');
+    });
+};
+
+cardAll.forEach(element => {
+    element.addEventListener("click", () => {
+        clearSelectCard();
+        element.classList.add('--is-select');
+    });
 });
