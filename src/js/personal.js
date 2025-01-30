94a523b87c5c0af5d94062c3dac7cd31b839b290
marryyx <markfrancyzov@gmail.com>
2025-01-30 15:38:53 +0200

    Work with js files is not finished

--- src/js/personal.js
+++ src/js/personal.js
@@ -15,81 +15,85 @@ const spanCounterSymbols = document.querySelector('.profile-form__counter-symbol
 const USER_AUTH_KEY = 'userAuthData';
 // ----------------------------------------
 
-const getUserAuthData = () => {
-    const data = localStorage.getItem(USER_AUTH_KEY)
-    return JSON.parse(data);
-};
-
-const counterSymbolsInput = () => {
-    spanCounterSymbols.textContent = textareaSymbols.value.length;
-};
-
-textareaSymbols.addEventListener('input', () => {
-    counterSymbolsInput();
-});
-
-const blockEventOnContent = (parm = false) => {
-    const main = document.querySelector('.profile-main');
-    const content = document.querySelector('.profile-main__content');
-
-    if (!parm) {
-        content.classList.remove('gm-not-event');
-        main.classList.remove('gm-scroll-hide');
-        return
-    }
-
-    main.classList.add('gm-scroll-hide');
-    content.classList.add('gm-not-event');
-};
-
-const activeInfoItem = (elm) => {
-    elm.classList.add('--active');
-};
-
-const inActiveInfoItems = () => {
-    cardItems.forEach(item => item.classList.remove('--active'));
-};
-
-const showEditForms = (activeForm) => {
-    const forms = dialogPersonal.querySelectorAll('.profile-form__section');
-
-    dialogPersonal.classList.remove('gm-hide');
-
-    forms.forEach(elm => {
-        const data = elm.dataset.targetDialog;
-
-        (data === activeForm)
-            ? elm.classList.remove('gm-hide')
-            : elm.classList.add('gm-hide')
+const personalPage = () => {
+    const getUserAuthData = () => {
+        const data = localStorage.getItem(USER_AUTH_KEY)
+        return JSON.parse(data);
+    };
+    
+    const counterSymbolsInput = () => {
+        spanCounterSymbols.textContent = textareaSymbols.value.length;
+    };
+    
+    textareaSymbols.addEventListener('input', () => {
+        counterSymbolsInput();
     });
-};
-
-const hideEditForms = () => {
-    dialogPersonal.classList.add('gm-hide');
-};
-
-cardItems.forEach(item => {
-    item.addEventListener('click', () => {
-        const targetElement = item.dataset.actionDialog;
-        showEditForms(targetElement);
-        blockEventOnContent(true);
-        activeInfoItem(item);
-    });
-});
-
-closeBtnDialog.addEventListener('click', () => {
-    hideEditForms();
-    inActiveInfoItems();
-
-    blockEventOnContent(false);
-});
-
-window.addEventListener('DOMContentLoaded', () => {
-    const dataUser = getUserAuthData();
-
-    if (dataUser) {
-        dataUser.forEach(item => {
-            blockNameUser.textContent = `${item.usernameFirst} ${item.usernameLast}`;
+    
+    const blockEventOnContent = (parm = false) => {
+        const main = document.querySelector('.profile-main');
+        const content = document.querySelector('.profile-main__content');
+    
+        if (!parm) {
+            content.classList.remove('gm-not-event');
+            main.classList.remove('gm-scroll-hide');
+            return
+        }
+    
+        main.classList.add('gm-scroll-hide');
+        content.classList.add('gm-not-event');
+    };
+    
+    const activeInfoItem = (elm) => {
+        elm.classList.add('--active');
+    };
+    
+    const inActiveInfoItems = () => {
+        cardItems.forEach(item => item.classList.remove('--active'));
+    };
+    
+    const showEditForms = (activeForm) => {
+        const forms = dialogPersonal.querySelectorAll('.profile-form__section');
+    
+        dialogPersonal.classList.remove('gm-hide');
+    
+        forms.forEach(elm => {
+            const data = elm.dataset.targetDialog;
+    
+            (data === activeForm)
+                ? elm.classList.remove('gm-hide')
+                : elm.classList.add('gm-hide')
         });
-    }
-});
+    };
+    
+    const hideEditForms = () => {
+        dialogPersonal.classList.add('gm-hide');
+    };
+    
+    cardItems.forEach(item => {
+        item.addEventListener('click', () => {
+            const targetElement = item.dataset.actionDialog;
+            showEditForms(targetElement);
+            blockEventOnContent(true);
+            activeInfoItem(item);
+        });
+    });
+    
+    closeBtnDialog.addEventListener('click', () => {
+        hideEditForms();
+        inActiveInfoItems();
+    
+        blockEventOnContent(false);
+    });
+    
+    window.addEventListener('DOMContentLoaded', () => {
+        const dataUser = getUserAuthData();
+    
+        if (dataUser) {
+            dataUser.forEach(item => {
+                blockNameUser.textContent = `${item.usernameFirst} ${item.usernameLast}`;
+            });
+        }
+    });
+}
+
+export { personalPage }
