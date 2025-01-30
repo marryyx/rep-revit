ad771aa6ea41363337c2bfa57e355b7343f164be
marryyx <markfrancyzov@gmail.com>
2025-01-30 15:38:39 +0200

    javascript +dialog content | last commit 18.08.2024

--- js/index.js
+++ js/index.js
@@ -101,15 +101,17 @@ updatePrice('first');
 cardAll[0].classList.add('--is-select');
 
 const btnBasket = document.querySelector('.header__btn-basket');
-const btnClose = document.querySelector('.basket-btn-close');
+const btnClose = document.querySelectorAll('[closeBasket]');
 const dialogBasket = document.querySelector('.basket');
 
 btnBasket.addEventListener('click', () => {
     dialogBasket.showModal();
 });
 
-btnClose.addEventListener('click', () => {
-    dialogBasket.close();
+btnClose.forEach(element => {
+    element.addEventListener('click', () => {
+        dialogBasket.close();
+    });
 });
 
 const addBasket = document.querySelector('.select-term__button-add');
@@ -118,4 +120,34 @@ const basket = document.querySelector('.header__btn-basket');
 addBasket.addEventListener('click', () => {
     const basketValue = parseInt(basket.dataset.basket);
     basket.dataset.basket = basketValue + 1;
+    addToBasket('Flex', '1 year', 100, 300);
 });
+
+const addToBasket = (type, term, quanity, price) => {
+    const basketContent = document.querySelector('.basket__content')
+    const typeImage = type ? 'coins.png' : 'revit.png';
+    const typeCaption = type ? 'Flex' : 'Revit';
+
+    const basket = 
+                `<div class="basket__content-card">
+                    <div class="basket__content-top">
+                        <p><img src="./image/${typeImage}" alt="#">${typeCaption}</p>
+                        <h3 class="total">$${price}</h3>
+                    </div>
+
+                    <div class="basket__content-title">
+                        <p>Term</p>
+                        <p>Quanity</p>
+                    </div>
+
+                    <div class="basket__content-subtitle">
+                        <h3>${term}</h3>
+                        <h3>${quanity}</h3>
+                    </div>
+
+                    <div class="basket__content-btn-remove">
+                        <button class="btn-remove">Remove</button>
+                    </div>
+                </div>`
+    basketContent.innerHTML = basket;
+} 
