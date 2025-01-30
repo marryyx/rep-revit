0dc5e62996da8038d90a909d5bdeb59479aa56f9
marryyx <markfrancyzov@gmail.com>
2025-01-30 15:38:36 +0200

    complete

--- js/index.js
+++ js/index.js
@@ -1,32 +1,6 @@
-const inputFirst = document.querySelector('#username');
-const inputSecond = document.querySelector('#symbol');
-const btn = document.querySelector('.button');
+const btn = document.querySelector('button')
+const box = document.querySelector('.box')
 
-const results = document.querySelector('.results')
-
-function replaceNum(text, tmp) {
-    const regex = /\d+/g;
-    const replaceText = text.replace(regex, tmp);
-
-    if (text === '') {
-        alert('Ошибка. Поле пустое!');
-
-        location.reload()
-    }
-
-    if (regex.test(tmp)) {
-        alert('есть числа во втором инпуте!'); 
-
-        return text;
-    }
-
-    return replaceText;
-}
-
-btn.addEventListener('click', function () {
-    const inputFirstResult = inputFirst.value;
-    const inputSecondResult = inputSecond.value;
-    const res = replaceNum(inputFirstResult, inputSecondResult);
-
-    results.innerHTML = res;
+btn.addEventListener("click", () => {
+    box.classList.toggle('show')
 });
