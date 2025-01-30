a69644ae3d36bd6aec9805102263c8c4d22ea1a7
marryyx <markfrancyzov@gmail.com>
2025-01-30 15:38:37 +0200

    ne gotovo

--- js/index.js
+++ js/index.js
@@ -3,17 +3,20 @@ const tabs = document.querySelectorAll('.buy-component__tab');
 const priceBlock = document.querySelector('.price-text-buy');
 const deskBlock = document.querySelector('.desk-text-buy');
 
-const INFORMATION = {
-    first: '/3 years for 1 user (pay annually)',
-    second: '/year for 1 user',
-    third: '/month for 1 user'
+const PLAN_PRICE = {
+    first: {
+        price: '$8,730',
+        description: '/3 years for 1 user (pay annually)',
+    },
+    second: {
+        price: '$2,910',
+        description: '/year for 1 user',
+    },
+    third: {
+        price: '$365',
+        description: '/month for 1 user',
+    }
 };
-
-// const PRICE = {
-//     first: '$8,730',
-//     second: '$2,910',
-//     third: '$365'
-// };
 
 const clearSelectTab = () => {
     tabs.forEach(element => {
@@ -34,28 +37,34 @@ const clearSelectCard = () => {
     });
 };
 
-const updatePrice = (key, price) => {
-    const priceBlock = document.querySelector('.price-text-buy');
-    const deskBlock = document.querySelector('.desk-text-buy');
+const updatePrice = (key) => {
+    price = PLAN_PRICE[key].price;
+    description = PLAN_PRICE[key].description;
+};
 
-    priceBlock.innerHTML = price;
-    deskBlock.textContent = INFORMATION[key];
+const updateContentOfPlan = (elm) => {
+    pricePlans = element.querySelector('p');
+    console.log('price =', price);
+    pricePlans.innerHTML = PLAN_PRICE.price;
 };
 
 cardAll.forEach(element => {
+    updateContentOfPlan();
+
     element.addEventListener("click", () => {
         clearSelectCard();
 
-        const price = element.querySelector('p').textContent;
         const data = element.dataset.select;
 
-        updatePrice(data, price);
+        updatePrice(data);
 
         priceBlock.innerHTML = price;
+        deskBlock.textContent = description;
 
         element.classList.add('--is-select');
     });
 });
 
-updatePrice('first', cardAll[0].querySelector('p').textContent);
+
+updatePrice('first');
 cardAll[0].classList.add('--is-select');
