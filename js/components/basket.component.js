56d2cdb2d4309670a51ac29ad7c16e8ce6bf5d38
marryyx <markfrancyzov@gmail.com>
2025-01-30 15:38:42 +0200

    last commiting 30.10.2024 23:45

--- js/components/basket.component.js
+++ js/components/basket.component.js
@@ -1,41 +1,42 @@
 export const basketComponent = () => {
 
-const btnBasket = document.querySelector('.header__btn-basket');
-const btnClose = document.querySelectorAll('[closeBasket]');
-const dialogBasket = document.querySelector('.basket');
+    const btnBasket = document.querySelector('.header__btn-basket');
+    const btnClose = document.querySelectorAll('[closeBasket]');
+    const dialogBasket = document.querySelector('.basket');
 
-btnBasket.addEventListener('click', () => {
-    dialogBasket.showModal();
-});
-
-btnClose.forEach(element => {
-    element.addEventListener('click', () => {
-        dialogBasket.close();
+    btnBasket.addEventListener('click', () => {
+        dialogBasket.showModal();
     });
-});
 
-const addBasket = document.querySelector('.select-term__button-add');
-const basket = document.querySelector('.header__btn-basket');
+    btnClose.forEach(element => {
+        element.addEventListener('click', () => {
+            dialogBasket.close();
+        });
+    });
 
-const getBasketCounter = () => {
-    return parseInt(btnBasket.dataset.basketCounter);
-};
+    const addBasket = document.querySelector('.select-term__button-add');
+    const basket = document.querySelector('.header__btn-basket');
 
-const setBasketCounter = (count) => {
-    btnBasket.dataset.basketCounter = count;
-};
+    const getBasketCounter = () => {
+        return parseInt(btnBasket.dataset.basketCounter);
+    };
 
-addBasket.addEventListener('click', () => {
-    addToBasket('Flex', '1 year', 100, 300);
-});
+    const setBasketCounter = (count) => {
+        btnBasket.dataset.basketCounter = count;
+    };
 
-const addToBasket = (type, term, quanity, price) => {
-    const basketContent = document.querySelector('.basket__content')
-    const typeImage = type ? 'coins.png' : 'revit.png';
-    const typeCaption = type ? 'Flex' : 'Revit';
+    addBasket.addEventListener('click', () => {
+        addToBasket('Flex', '1 year', 100, 300);
+        dialogBasket.showModal();
+    });
 
-    const basket =
-        `<div class="basket__content-card">
+    const addToBasket = (type, term, quanity, price) => {
+        const basketContent = document.querySelector('.basket__content')
+        const typeImage = type ? 'coins.png' : 'revit.png';
+        const typeCaption = type ? 'Flex' : 'Revit';
+
+        const basket =
+            `<div class="basket__content-card">
                     <div class="basket__content-top">
                         <p><img src="./image/${typeImage}" alt="#">${typeCaption}</p>
                         <h3 class="total">$${price}</h3>
@@ -56,9 +57,20 @@ const addToBasket = (type, term, quanity, price) => {
                     </div>
                 </div>`;
 
-    basketContent.insertAdjacentHTML("beforeend", basket);
+        basketContent.insertAdjacentHTML("beforeend", basket);
 
-    const itemsLenght = basketContent.querySelectorAll('.basket__content-card').length;
-    setBasketCounter(itemsLenght);
-};
+        const basketContentCard = document.querySelector('.basket__content-card')
+        const basketButtonRemove = document.querySelector('.basket__content-card .btn-remove');
+        basketButtonRemove.addEventListener('click', () => {
+            basketContentCard.remove();
+
+            const itemsLenght = basketContent.querySelectorAll('.basket__content-card').length;
+            setBasketCounter(itemsLenght);
+        });
+
+        const itemsLenght = basketContent.querySelectorAll('.basket__content-card').length;
+        setBasketCounter(itemsLenght);
+        
+        localStorage.setItem(basketContent, itemsLenght);
+    };
 }
