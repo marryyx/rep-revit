621f0cc5281df9d5a2efab3d3a5bc4e746a1609a
marryyx <markfrancyzov@gmail.com>
2025-01-30 15:38:37 +0200

    complete add flex tab +js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 const cardAll = document.querySelectorAll('.select-term__card');
+const tabContent = document.querySelectorAll('.buy-component__tabs-content');
+const tabFooterContent = document.querySelectorAll('.buy-component__footer')
 const tabs = document.querySelectorAll('.buy-component__tab');
 const priceBlock = document.querySelector('.price-text-buy');
 const deskBlock = document.querySelector('.desk-text-buy');
+
+const priceCard = document.querySelector('#price-buy-2');
+const btnInfo = document.querySelector('.buttons-buy-tokens')
 
 const PLAN_PRICE = {
     first: {
@@ -18,16 +23,44 @@ const PLAN_PRICE = {
     }
 };
 
+const PLAN_PRICE_TWO = {
+    first: {
+        price: '$300',
+        btn: 'Buy tokens'
+    },
+    second: {
+        price: '$1,500',
+        btn: 'Buy tokens'
+    },
+    third: {
+        price: 'Varies',
+        btn: 'Estimate tokens needed'
+    }
+};
+
 const clearSelectTab = () => {
     tabs.forEach(element => {
         element.classList.remove('--is-active');
     });
 };
 
+const selectTabContent = (name, content, classHide) =>
+    content.forEach(item => {
+        const tabContent = item.dataset.tab
+        if (name != tabContent) {
+            item.classList.add(classHide)
+        } else { item.classList.remove(classHide) }
+    });
+
+
 tabs.forEach(element => {
     element.addEventListener("click", () => {
         clearSelectTab();
         element.classList.add('--is-active');
+        const tabNane = element.dataset.tab;
+
+        selectTabContent(tabNane, tabContent, '--is-hide');
+        selectTabContent(tabNane, tabFooterContent, '--footer-hide');
     });
 });
 
@@ -41,6 +74,11 @@ const updatePrice = (key) => {
     const price = PLAN_PRICE[key].price;
     const description = PLAN_PRICE[key].description;
 
+    const priceTwo = PLAN_PRICE_TWO[key].price;
+    const btnDesk = PLAN_PRICE_TWO[key].btn;
+
+    btnInfo.innerHTML = btnDesk;
+    priceCard.innerHTML = priceTwo;
     priceBlock.innerHTML = price;
     deskBlock.textContent = description;
 };
@@ -49,7 +87,6 @@ const updateContentOfPlan = (elm) => {
     const pricePlans = elm.querySelector('p');
     const key = elm.dataset.select;
 
-    // console.log('price =', pricePlans);
     pricePlans.innerHTML = PLAN_PRICE[key].price;
 };
 
@@ -58,13 +95,36 @@ cardAll.forEach(element => {
 
     element.addEventListener("click", () => {
         clearSelectCard();
-        
+
         updatePrice(element.dataset.select);
 
         element.classList.add('--is-select');
     });
 });
 
+// const updatePriceTwo = (key) => {
+//     const price = PLAN_PRICE_TWO[key].price;
+//     priceCard.innerHTML = price;
+// };
+
+// const updateContentOfPlanTwo = (elm) => {
+//     const pricePlans = elm.querySelector('p');
+//     const key = elm.dataset.select;
+
+//     pricePlans.innerHTML = PLAN_PRICE_TWO[key].price;
+// };
+
+// cardAll.forEach(element => {
+//     updateContentOfPlanTwo(element);
+
+//     element.addEventListener("click", () => {
+//         clearSelectCard();
+
+//         updatePriceTwo(element.dataset.select);
+
+//         element.classList.add('--is-select');
+//     });
+// });
 
 updatePrice('first');
 cardAll[0].classList.add('--is-select');
