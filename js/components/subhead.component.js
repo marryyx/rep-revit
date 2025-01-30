dd4bfd2b75687035ffe0d39bdd4a111306214d1c
marryyx <markfrancyzov@gmail.com>
2025-01-30 15:38:44 +0200

    bugs subhead content | 14.11.2024

--- js/components/subhead.component.js
+++ js/components/subhead.component.js
@@ -2,6 +2,10 @@ export const subheadComponent = () => {
     const burgerBtn = document.querySelector('.subhead__burger');
 
     burgerBtn.addEventListener('click', () => {
+        const hiddenItems = document.querySelector('.subhead__item.--industries')
+
         burgerBtn.classList.toggle('--open');
+
+        hiddenItems.classList.toggle('--open');
     });
 }
