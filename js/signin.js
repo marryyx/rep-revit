611fac84b388bf5ebe555147efe5e5825baf4e6c
marryyx <markfrancyzov@gmail.com>
2025-01-30 15:38:46 +0200

    last commit 05.12.2024 markk

--- js/signin.js
+++ js/signin.js
@@ -1,13 +1,13 @@
 const wrapperAuth = document.querySelector('.auth');
 
-const emailInput = wrapperAuth.querySelector('#email');
-
+const email = wrapperAuth.querySelector('#email');
 const buttonSumbit = wrapperAuth.querySelector('#sumbit');
 
 // -----------------
 
 const USER_DATA_KEY = 'userData';
 const USER_DATA = [];
+const USER_DATA_RED = {};
 
 // -----------------
 
@@ -16,24 +16,16 @@ const getDataUserOfStore = () => {
     return JSON.parse(dataUser)
 };
 
-// console.log('ДАННЫЕ ЮЗЕРА =', getDataUserOfStore());
-
 function logInInputCheked() {
     const users = getDataUserOfStore();
+    // const message = wrapperAuth.querySelector('.auth__message');
+    const value = email.value;
 
     users.forEach(item => {
-        const message = wrapperAuth.querySelector('.auth__message');
-        const notEmail = wrapperAuth.querySelector('.auth__not-email');
-        const value = emailInput.value;
-
-        if (!value || (value != value)) {
-            notEmail.classList.add('--message-email');
-            setTimeout(() => notEmail.classList.remove('--message-email'), 5000);
-        } else if (value != item.email) {
-            message.classList.add('--message-error');
-            setTimeout(() => message.classList.remove('--message-error'), 5000);
+        if (value != item.email) {
+            console.log('нет')
         } else if (value === item.email) {
-            // window.location.href = 'signin-password.html';
+            console.log('да')
         }
     });
 }
@@ -41,3 +33,7 @@ function logInInputCheked() {
 buttonSumbit.addEventListener('click', () => {
     logInInputCheked();
 });
+
+// document.addEventListener("DOMContentLoaded", (event) => {
+
+// });
