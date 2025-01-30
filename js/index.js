c563042f0c7397e4dd807ab0c419a4b1bb392f8a
marryyx <markfrancyzov@gmail.com>
2025-01-30 15:38:40 +0200

    complete section ar-imagine | last commit 11.09.2024

--- js/index.js
+++ js/index.js
@@ -184,3 +184,36 @@ closeVideo.forEach(element => {
         vidTwo.pause();
     });
 });
+
+const video = document.querySelectorAll('.ar-imagine__image-video img');
+const footerVideos = document.querySelectorAll('.ar-imagine__footer');
+const videos = document.querySelectorAll('.ar-imagine__video');
+
+const clearSelectVideo = (element, newClass) => {
+    element.forEach(elm => {
+        elm.classList.remove(newClass);
+    });
+};
+
+const selectVideoTab = (name, content, classHide) =>
+    content.forEach(item => {
+        const videoFooter = item.dataset.video
+
+        if (name != videoFooter) {
+            item.classList.add(classHide)
+        } else {
+            item.classList.remove(classHide)
+        }
+    });
+
+
+video.forEach(element => {
+    element.addEventListener("click", () => {
+        clearSelectVideo(video, '--select')
+        element.classList.add('--select');
+
+        const tabVideo = element.dataset.video;
+        selectVideoTab(tabVideo, videos, '--hide-video');
+        selectVideoTab(tabVideo, footerVideos, '--hide');
+    });
+});
